4c03abe5379e3692280209b32eaf752509c05525
HanSeongLee <jkhanseong@gmail.com>
2023-01-31 19:43:53 +0900

    ADD | InputField component

---
+++ components/InputField/index.tsx
@@ -0,0 +1,39 @@
+import React, { InputHTMLAttributes } from 'react';
+import styles from './style.module.scss';
+import cn from 'classnames';
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+    label: string;
+    error?: string;
+}
+
+const InputField: React.FC<IProps> = ({
+                                          label, error, id, className,
+                                          ...props
+                                      }) => {
+    return (
+        <div className={cn(styles.inputField, {
+            [styles.error]: error,
+        }, className)}>
+            <div className={styles.labelContainer}>
+                <label className={styles.label}
+                       htmlFor={id}
+                >
+                    {label}
+                </label>
+                {error && (
+                    <div className={styles.errorMessage}>
+                        {error}
+                    </div>
+                )}
+            </div>
+            <input className={styles.input}
+                   id={id}
+                   type={'text'}
+                   {...props}
+            />
+        </div>
+    );
+};
+
+export default InputField;
