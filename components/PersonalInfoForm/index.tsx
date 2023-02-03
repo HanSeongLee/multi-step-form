e388b903987d276fafe201f1fcdd7a3fcbf46aa5
HanSeongLee <jkhanseong@gmail.com>
2023-02-03 18:12:59 +0900

    ADD | Style of PersonalInfoForm component on desktop

--- components/PersonalInfoForm/index.tsx
+++ components/PersonalInfoForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { FormHTMLAttributes } from 'react';
+import styles from './style.module.scss';
 import Form from 'components/common/Form';
 import InputField from 'components/common/InputField';
 import { Control } from 'react-hook-form/dist/types/form';
@@ -16,7 +17,8 @@ const defaultInputRule = {
 
 const PersonalInfoForm: React.FC<IProps> = ({ control, ...props }) => {
     return (
-        <Form title={'Personal info'}
+        <Form className={styles.personalInfoForm}
+              title={'Personal info'}
               description={'Please provide your name, email address, and phone number.'}
               sidebarProps={{
                   current: 0,
@@ -24,45 +26,47 @@ const PersonalInfoForm: React.FC<IProps> = ({ control, ...props }) => {
               }}
               {...props}
         >
-            <Controller name={'name'}
-                        control={control}
-                        rules={defaultInputRule}
-                        render={({ field, fieldState: { error } }) => (
-                            <InputField id={'name'}
-                                        label={'Name'}
-                                        placeholder={'e.g. Stephen King'}
-                                        error={error?.message}
-                                        {...field}
-                            />
-                        )}
-            />
-            <Controller name={'email'}
-                        control={control}
-                        rules={defaultInputRule}
-                        render={({ field, fieldState: { error } }) => (
-                            <InputField id={'email'}
-                                        type={'email'}
-                                        label={'Email Address'}
-                                        placeholder={'e.g. stephenking@lorem.com'}
-                                        error={error?.message}
-                                        {...field}
-                            />
-                        )}
-            />
-            <Controller name={'phone'}
-                        control={control}
-                        rules={defaultInputRule}
-                        render={({ field, fieldState: { error } }) => (
-                            <InputField id={'phone'}
-                                        type={'tel'}
-                                        pattern={'^(\\+\\d{1,2}\\s)?\\(?\\d{2,3}\\)?[\\s]\\d{3,4}[\\s]\\d{3,4}$'}
-                                        label={'Phone Number'}
-                                        placeholder={'e.g. +1 234 567 890'}
-                                        error={error?.message}
-                                        {...field}
-                            />
-                        )}
-            />
+            <div className={styles.container}>
+                <Controller name={'name'}
+                            control={control}
+                            rules={defaultInputRule}
+                            render={({ field, fieldState: { error } }) => (
+                                <InputField id={'name'}
+                                            label={'Name'}
+                                            placeholder={'e.g. Stephen King'}
+                                            error={error?.message}
+                                            {...field}
+                                />
+                            )}
+                />
+                <Controller name={'email'}
+                            control={control}
+                            rules={defaultInputRule}
+                            render={({ field, fieldState: { error } }) => (
+                                <InputField id={'email'}
+                                            type={'email'}
+                                            label={'Email Address'}
+                                            placeholder={'e.g. stephenking@lorem.com'}
+                                            error={error?.message}
+                                            {...field}
+                                />
+                            )}
+                />
+                <Controller name={'phone'}
+                            control={control}
+                            rules={defaultInputRule}
+                            render={({ field, fieldState: { error } }) => (
+                                <InputField id={'phone'}
+                                            type={'tel'}
+                                            pattern={'^(\\+\\d{1,2}\\s)?\\(?\\d{2,3}\\)?[\\s]\\d{3,4}[\\s]\\d{3,4}$'}
+                                            label={'Phone Number'}
+                                            placeholder={'e.g. +1 234 567 890'}
+                                            error={error?.message}
+                                            {...field}
+                                />
+                            )}
+                />
+            </div>
         </Form>
     );
 };
