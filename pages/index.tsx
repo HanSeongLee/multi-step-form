0781e41743fdbb02c90e0ae4ebe61e91f575d4c4
HanSeongLee <jkhanseong@gmail.com>
2023-02-03 18:34:10 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Container from 'components/common/Container';
+import MultiStepFormContainer from 'containers/MultiStepFormContainer';
 
 const Home: NextPage = () => {
   return (
       <>
-        <main>
+        <main className={styles.main}>
+            <Container className={styles.container}>
+                <MultiStepFormContainer />
+            </Container>
         </main>
       </>
   );
